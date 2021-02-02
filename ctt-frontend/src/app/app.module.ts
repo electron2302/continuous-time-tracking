2a5b053c42f0f0a85b65dc4a354a0161b50d813d
AndHager <andreas.polling@web.de>
2021-02-02 17:46:58 +0100

    imported amplify ui module

--- ctt-frontend/src/app/app.module.ts
+++ ctt-frontend/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyUIAngularModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
