2cc00ba449e84860a9aead02d1ce1040ae498373
walli545 <walli5446@googlemail.com>
2021-02-03 18:50:51 +0100

    API: Add StatisticType enum

--- ctt-frontend/src/models/index.js
+++ ctt-frontend/src/models/index.js
@@ -2,11 +2,15 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
-
+const StatisticType = {
+  "RELATIVE_TIME": "RelativeTime",
+  "ABSOLUTE_TIME": "AbsoluteTime"
+};
 
 const { Category, Activity } = initSchema(schema);
 
 export {
   Category,
-  Activity
+  Activity,
+  StatisticType
 };
