4d3c1db0cf8136311e42d5cdeeab1ce188a1d381
Sven Schötz <sven.schoetz@kabelmail.de>
2021-02-05 11:48:02 +0100

    Add tests for create function.

--- ctt-frontend/src/app/services/aws-category.service.spec.ts
+++ ctt-frontend/src/app/services/aws-category.service.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import * as TypeMoq from 'typemoq';
+import { APIService } from './API.service';
+import { CreateCategoryInput } from './category.service'
 
 import { AwsCategoryService } from './aws-category.service';
+import { CreateCategoryInput as APICreateInput } from './API.service';
+import { CreateCategoryMutation as APICreateMutation } from './API.service';
 
 describe('AwsCategoryService', () => {
   let service: AwsCategoryService;
@@ -15,12 +20,50 @@ describe('AwsCategoryService', () => {
   });
 
   describe('create', () => {
-    it('should ', () => {
+    it('should accept a CreateCategoryInput', () => {
+      const mut: APICreateMutation = {
+        color: '#000000',
+        name: 'TestCat',excludeFromStatistics: [],
+        reminderInterval: 10,
+        activities: null,
+        __typename: 'Category',
+        id: '1',
+        createdAt: '',
+        owner: null,
+        _deleted: false,
+        _lastChangedAt: 0,
+        _version: 1,
+        updatedAt: ''
+      };
 
+      const apiMock = TypeMoq.Mock.ofType(APIService);
+      apiMock.setup(x => x.CreateCategory(TypeMoq.It.is(y => (y as APICreateInput) !== null))).returns((y) => Promise.resolve(mut));
+      const category: CreateCategoryInput = {
+        color: '#00000',
+        name: 'TestCat',
+        excludeFromStatistics: [],
+        reminderInterval: 10
+      };
+      const sut = new AwsCategoryService(apiMock.object);
+
+      sut.create(category);
+
+      expect(apiMock.verify(x => x.CreateCategory(TypeMoq.It.isAny()), TypeMoq.Times.exactly(1))).toBeUndefined();
     });
 
-    it('should ', () => {
+    it('should reject promise if category could not be created', async function() {
+      const apiMock = TypeMoq.Mock.ofType(APIService);
+      apiMock.setup(x => x.CreateCategory(TypeMoq.It.is(y => (y as APICreateInput) !== null))).returns((y) => Promise.reject());
+      const category: CreateCategoryInput = {
+        color: '#000000',
+        name: 'TestCat',
+        excludeFromStatistics: [],
+        reminderInterval: 10
+      };
+      const sut = new AwsCategoryService(apiMock.object);
 
+      await expectAsync(sut.create(category)).toBeRejectedWith('Category TestCat could not be added');
+      expect(apiMock.verify(x => x.CreateCategory(TypeMoq.It.isAny()), TypeMoq.Times.exactly(1))).toBeUndefined();
     });
   });
 
